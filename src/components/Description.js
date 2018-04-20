cd65bbfa305d2f5c910868ce1ce9304015f7e120
Ahmed Hosny <i.ahmedhosny@gmail.com>
2018-04-20 00:12:56 -0400

    adjusts image size

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Glamorous from "glamorous";
+import Grid from "material-ui/Grid";
+
 /**
  * The component
  * @type {Object}
@@ -23,42 +25,51 @@ class Description extends Component {
     });
     return (
       <GDiv>
-        The <GStrong>Dataset Nutrition Label</GStrong> project aims to create a
-        standard for interrogating datasets for measures that will ultimately
-        drive the creation of better, more inclusive machine learning models.
-        Our current prototype includes several ‘modules’ across a variety of
-        qualitative and quantitative data that we believe is useful for
-        exploring several aspects in datasets before the development of models.
-        <br />
-        <br />
-        <img
-          src={"img/poster+figures_22.png"}
-          style={{ width: "100%", opacity: 0.75 }}
-        />
-        <br />
-        <br />
-        We developed this prototype on ProPublica’s Dollars for Docs (2013-2015){" "}
-        <a href="https://projects.propublica.org/docdollars/" target="_blank">
-          dataset
-        </a>, which details payments made from pharmaceutical companies to
-        doctors. You can navigate through the modules using the links on the
-        left.
-        <br />
-        <br />
-        To learn more about our project, please visit our{" "}
-        <a href="http://datanutrition.media.mit.edu/" target="_blank">
-          website
-        </a>, read our{" "}
-        <a
-          href="http://datanutrition.media.mit.edu/abstract.pdf"
-          target="_blank"
-        >
-          paper abstract
-        </a>
-        , or email us at{" "}
-        <a href="mailto:nutrition@media.mit.edu" target="_blank">
-          nutrition@media.mit.edu
-        </a>.
+        <Grid container spacing={24}>
+          <Grid item xs={12} sm={12} md={12}>
+            The <GStrong>Dataset Nutrition Label</GStrong> project aims to
+            create a standard for interrogating datasets for measures that will
+            ultimately drive the creation of better, more inclusive machine
+            learning models. Our current prototype includes several ‘modules’
+            across a variety of qualitative and quantitative data that we
+            believe is useful for exploring several aspects in datasets before
+            the development of models.
+            <br />
+            <br />
+            <img
+              src={"img/poster+figures_22.png"}
+              style={{ width: "100%", opacity: 0.75 }}
+            />
+            <br />
+            <br />
+            We developed this prototype on ProPublica’s Dollars for Docs
+            (2013-2015){" "}
+            <a
+              href="https://projects.propublica.org/docdollars/"
+              target="_blank"
+            >
+              dataset
+            </a>, which details payments made from pharmaceutical companies to
+            doctors. You can navigate through the modules using the links on the
+            left.
+            <br />
+            <br />
+            To learn more about our project, please visit our{" "}
+            <a href="http://datanutrition.media.mit.edu/" target="_blank">
+              website
+            </a>, read our{" "}
+            <a
+              href="http://datanutrition.media.mit.edu/abstract.pdf"
+              target="_blank"
+            >
+              paper abstract
+            </a>
+            , or email us at{" "}
+            <a href="mailto:nutrition@media.mit.edu" target="_blank">
+              nutrition@media.mit.edu
+            </a>.
+          </Grid>
+        </Grid>
       </GDiv>
     );
   }
