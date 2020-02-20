a09d2a9df37df94be8be4e7f75f41c7a880ab3e0
Ahmed Hosny <i.ahmedhosny@gmail.com>
2020-02-20 10:10:50 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
         <header className="App-header">
           <img src={"img/logo-s.png"} style={{ height: "100%" }} />
           <div style={{ height: "100%", flex: 1 }}>
-            <h1 className="App-title">Dataset Nutrition Label Prototype</h1>
+            <h1 className="App-title">Dataset Nutrition Label</h1>
           </div>
         </header>
         <Viewer data={data} raw={raw} />
